e16d90e3b46d5d01d2c3bdda0647f7d72fbfcde7
Hao Lu <hl1319@ic.ac.uk>
2020-02-24 22:16:07 +0000

    update shoaling initialisation

--- src/SPH_2D.cpp
+++ src/SPH_2D.cpp
@@ -19,10 +19,10 @@ void SPH_main::set_values(void)
 	min_x[0] = 0.0;
 	min_x[1] = 0.0;
 
-	max_x[0] = 1.0;
-	max_x[1] = 1.0;
+	max_x[0] = 20.0;
+	max_x[1] = 10.0;
 
-	dx = 0.02;
+	dx = 1;
 	
 	h_fac = 1.3;
 	h = dx*h_fac;
@@ -33,7 +33,8 @@ void SPH_main::initialise_grid(void)
 	for (int i = 0; i < 2; i++)
 	{
 		min_x[i] -= 2.0*h;
-		max_x[i] += 2.0*h;												//add buffer for virtual wall particles
+		max_x[i] += 2.0*h;
+        //add buffer for virtual wall particles
 
 		max_list[i] = int((max_x[i] - min_x[i]) / (2.0*h) + 1.0);
 	}
@@ -54,10 +55,10 @@ void SPH_main::place_points(double *min, double *max, string shape)
     
     if (shape == "rectangle")
     {
-        while (x[0] <= max[0])
+        while (x[1] <= max[1])
         {
-            x[1] = min[1];
-            while (x[1] <= max[1])
+            x[0] = min[0];
+            while (x[0] <= max[0])
             {
                 if ((x[0] < min[0] + 2.*h) || (x[0] > max[0] - 2.*h) || (x[1] < min[1] + 2.*h) || (x[1] > max[1] - 2.*h)) {
                     for (int i = 0; i < 2; i++)
@@ -65,7 +66,7 @@ void SPH_main::place_points(double *min, double *max, string shape)
                     inner_particle.calc_index();
                     particle_list.push_back(inner_particle);
                     cout << inner_particle.boundary_particle;
-                    x[1] += dx;
+                    x[0] += dx;
                 }
                 else {
                     for (int i = 0; i < 2; i++)
@@ -73,11 +74,64 @@ void SPH_main::place_points(double *min, double *max, string shape)
                     outer_particle.calc_index();
                     particle_list.push_back(outer_particle);
                     cout << outer_particle.boundary_particle;
-                    x[1] += dx;
+                    x[0] += dx;
                 }
             }
             cout << endl;
-            x[0] += dx;
+            x[1] += dx;
+        }
+    }
+    else if (shape == "shoaling")
+    {
+        // beach starts at half way in x1: (x1, 2h)
+        double shoaling_x1_start = (max[0] - 2.*h) / 2;
+        double shoaling_x1_end = max[0] - 2.*h;
+        // beach ends at (max[0] - 2h, x2)
+        double shoaling_x2_start = 0;
+        double shoaling_x2_end = (max[1] - 2.*h) / 5;
+        
+        const double shoaling_slope = (shoaling_x2_end - shoaling_x2_start) / (shoaling_x1_end - shoaling_x1_start);
+        cout << shoaling_slope << endl;
+        const double start[2] = { shoaling_x1_start, shoaling_x2_start };
+        const double end[2] = { shoaling_x1_end, shoaling_x2_end };
+        while (x[1] <= max[1])
+        {
+            x[0] = min[0];
+            while (x[0] <= max[0])
+            {
+                if ((x[0] < min[0] + 2.*h) || (x[0] > max[0] - 2.*h) || (x[1] < min[1] + 2.*h) || (x[1] > max[1] - 2.*h)) {
+                    for (int i = 0; i < 2; i++)
+                        inner_particle.x[i] = x[i];
+                    inner_particle.calc_index();
+                    particle_list.push_back(inner_particle);
+                    cout << inner_particle.boundary_particle;
+                    x[0] += dx;
+                }
+                else if ((x[0] >= shoaling_x1_start) && (x[0] <= shoaling_x1_end) && (x[1] >= shoaling_x2_start) && (x[1] <= shoaling_x2_end))
+                {
+                    for (int i = 0; i < 2; i++)
+                        outer_particle.x[i] = x[i];
+                    outer_particle.calc_index();
+                    particle_list.push_back(outer_particle);
+                    cout << inner_particle.boundary_particle;
+                    x[0] += dx;
+                }
+                else {
+                    for (int i = 0; i < 2; i++)
+                        outer_particle.x[i] = x[i];
+                    outer_particle.calc_index();
+                    particle_list.push_back(outer_particle);
+                    cout << outer_particle.boundary_particle;
+                    x[0] += dx;
+                }
+            }
+            cout << endl;
+            if ((x[1] >= start[1]) && x[1] <= end[1])
+            {
+                shoaling_x1_start += shoaling_slope * (end[0] - end[1]);
+                shoaling_x2_start += dx;
+            }
+            x[1] += dx;
         }
     }
 }

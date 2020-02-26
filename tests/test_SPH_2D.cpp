f0baf966e52b6a2c3b11d72b30d102bc0f4b572c
acse-gc2016 <chatzigeos@gmail.com>
2020-02-26 18:08:55 +0000

    fixed the pressure functions - added test for Pressure

--- tests/test_SPH_2D.cpp
+++ tests/test_SPH_2D.cpp
@@ -2,7 +2,40 @@
 #include "file_writer.h"
 #include <string>
 #include <iostream>
-#include <chrono>
+#include <cassert>
+
+// void test_timestep(int iterations = 3, SPH_main& domain) {
+// 	const double t_max = iterations*domain.dt;
+// 	std::cout << "dt = " << domain.dt << std::endl;
+// 	write_file("particles_" + std::to_string(domain.t) + ".vtp", domain.particle_list);
+
+// 	while(domain.t < t_max) {
+// 		domain.timestep();
+// 		write_file("particles_" + std::to_string(domain.t) + ".vtp", domain.particle_list);
+// 	}
+// 	write_file("particles_" + std::to_string(domain.t) + ".vtp", domain.particle_list);
+// }
+
+void test_P(SPH_main& domain) {
+		//for a single particle
+	auto particle = domain.particle_list.front();
+	//cout<<"domain rho0 "<<domain.rho0<<"\n";
+	particle.rho = 0;
+	//cout<<" rho shoudl be 0: "<<particle.rho<<"\n";
+	//cout<<"Particle Pressure " <<particle.P <<"\n";
+	particle.redef_P();
+	//cout<<"Particle Pressure after redef " <<particle.P <<"\n";
+	//cout <<"Abs:" <<abs(1 - particle.P / (-57142.9))<<"\n";
+	assert (abs(1 - particle.P / (-57142.9)) < 1e-6);
+	//cout <<"next:" <<abs(particle.P - (-57142.9))<<"\n";
+	assert (abs(particle.P - (-57142.9)) < 0.1);
+	 particle.rho = 1000;
+	 particle.redef_P();
+	 //cout<<"Particle Pressure before 0 " <<particle.P <<"\n";
+	 assert (particle.P == 0);
+
+
+}
 
 int main(int argc, char* argv[])
 {
@@ -13,25 +46,17 @@ int main(int argc, char* argv[])
 	domain.initialise_grid();									//initialise simulation grid
 
 	domain.place_points(domain.min_x, domain.max_x);				//places initial points - will need to be modified to include boundary points and the specifics of where the fluid is in the domain
-	auto first_particle = domain.particle_list.front();
-	write_file("particles_0.vtp", domain.particle_list);
+	
+	test_P(domain);
+	//test_timestep(domain);
 
-	int iterations = 3;
-	if (argc > 1) iterations = std::stoi(argv[1]);
-	const double t_max = iterations*domain.dt;
-	std::cout << "dt = " << domain.dt << std::endl;
-
-	const auto start = std::chrono::high_resolution_clock::now();
-	while(domain.t < t_max) {
-		domain.timestep();
-		if (domain.count % domain.output_intervval == 0)
-		{
-			write_file("particles_" + std::to_string(domain.t) + ".vtp", domain.particle_list);
-		}
-	}
-	const auto end = std::chrono::high_resolution_clock::now();
-	std::cout << "runtime [s] = " << (end-start).count() / 1e9 << endl;
-	write_file("particles_" + std::to_string(domain.t) + ".vtp", domain.particle_list);
+	// if (argc > 1) {
+	// 	test_timestep(std::stoi(argv[1]), domain);
+	// }
+	// else {
+	// 	//test_timestep(domain)
+	// }
+	
 	
 	return 0;
 }
